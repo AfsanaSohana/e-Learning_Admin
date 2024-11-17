b7021f5463804d71b48733b79801d0633dfe6815
sohana <sohanaafsana16@gmail.com>
2024-11-17 23:42:19 +0600

    courseinterface

---
+++ src/pages/Quiz/index.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AdminLayout from '../../layout/adminLayout';
+import { Link } from 'react-router-dom';
+
+
+function Quiz() {
+
+    const[data, setData]=useState([]);
+    useEffect(() => {
+        getDatas();
+    }, []);
+
+    function getDatas() {
+        axios.get(`${process.env.REACT_APP_API_URL}/module`).then(function(response) {
+            setData(response.data.data);
+        });
+    }
+    const deleteData = (id) => {
+        axios.delete(`${process.env.REACT_APP_API_URL}/module/${id}`).then(function(response){
+            getDatas();
+        });
+    }
+  return (
+    <AdminLayout>
+        <div className="row g-4">
+            <div className="col-sm-12">
+                <div className="bg-light rounded h-100 p-4">
+                    <h6 className="mb-4">Quiz</h6>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                          <th scope="col">Question</th>
+                                        <th scope="col">Options</th>
+                                        <th scope="col">Answer</th>
+                                        <th scope="col">Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                                    {data && data.map((d, key) =>
+                                        <tr key={d.id}>
+                                            <td>{d.question}</td>
+                                            <td>{d.options}</td>
+                                            <td>{d.correct_answer}</td>
+                                            <td>
+                                                <Link to={`/module/edit/${d.id}`} className='btn btn-info' >Edit</Link>
+                                                <button type='button' onClick={() => deleteData(d.id)} className='btn btn-danger'>Delete</button>
+                                            </td>
+                                        </tr>
+                                    )}
+                                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
+  )
+}
+
+export default Quiz
