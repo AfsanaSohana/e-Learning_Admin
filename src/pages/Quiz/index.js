44cad7fb8e2c48f9e5cbbf07d7d805383e36a238
AfsanaSohana <sohanaafsana16@gmail.com>
2024-11-26 13:30:53 +0600

    3rt commit

---
+++ src/pages/Quiz/index.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AdminLayout from '../../layout/adminLayout';
+import { Link } from 'react-router-dom';
+
+
+function Quiz() {
+
+    const[data, setData]=useState([]);
+    useEffect(() => {
+        getDatas();
+    }, []);
+
+    function getDatas() {
+        axios.get(`${process.env.REACT_APP_API_URL}/quiz`).then(function(response) {
+            setData(response.data.data);
+        });
+    }
+    const deleteData = (id) => {
+        axios.delete(`${process.env.REACT_APP_API_URL}/quiz/${id}`).then(function(response){
+            getDatas();
+        });
+    }
+  return (
+    <AdminLayout>
+        <div className="row g-4">
+            <div className="col-sm-12">
+                <div className="bg-light rounded h-100 p-4">
+                    <h6 className="mb-4">Quiz</h6>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Course ID</th>
+                                <th scope="col">Student ID</th>
+                                <th scope="col">Question</th>
+                                <th scope="col">Option-1</th>
+                                <th scope="col">Option-2</th>
+                                <th scope="col">Option-3</th>
+                                <th scope="col">Option-4</th>
+                                <th scope="col">Answer</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data && data.map((d, key) =>
+                                <tr key={d.id}>
+                                    <td>{d.course?.course_name}</td>
+                                    <td>{d.batch?.batch_name}</td>
+                                    <td>{d.module_name}</td>
+                                    <td>{d.video_id}</td>
+                                    <td>
+                                        <Link to={`/quiz/edit/${d.id}`} className='btn btn-info' >Edit</Link>
+                                        <button type='button' onClick={() => deleteData(d.id)} className='btn btn-danger'>Delete</button>
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
+  )
+}
+
+export default Quiz
